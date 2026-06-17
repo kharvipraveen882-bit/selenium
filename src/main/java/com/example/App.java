91fd802f2346ab47e8a98587af1d7a2be2d228b0
kharvipraveen882-bit <kharvipraveen882@gmail.com>
2026-06-17 11:01:58 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,22 +1,43 @@
 package com.example;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-       ChromeOptions options=new ChromeOptions();
-       options.addArguments("--headless");
-       options.addArguments("--no-sandbox");
-       options.addArguments("--disable-dev-shm-usage");
-       WebDriver driver=new ChromeDriver(options);
-       driver.get("https://www.saucedemo.com/");
-       driver.manage().window().maximize();
-       driver.findElement(By.id("user-name")).sendKeys("standard_user");
-       driver.findElement(By.id("password")).sendKeys("secret_sauce");
-       driver.findElement(By.id("login-button")).click();
+public class App {
+
+    public static void main(String[] args) {
+
+        ChromeOptions options = new ChromeOptions();
+
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        try {
+            driver.get("https://www.saucedemo.com/");
+
+            driver.findElement(By.id("user-name"))
+                  .sendKeys("standard_user");
+
+            driver.findElement(By.id("password"))
+                  .sendKeys("secret_sauce");
+
+            driver.findElement(By.id("login-button"))
+                  .click();
+
+            System.out.println("Login Successful");
+            System.out.println("Current URL: " + driver.getCurrentUrl());
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            driver.quit();
+        }
     }
 }
